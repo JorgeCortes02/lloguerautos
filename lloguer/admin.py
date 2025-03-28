b7d649e245c6b890ef98ae05610a1906b9d5d6ec
Jorge Cortés <jorgecortes@MacBook-Pro-de-Jorge.local>
2025-03-28 15:44:09 +0100

    fita2

--- lloguer/admin.py
+++ lloguer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class AutomobilAdmin(admin.ModelAdmin):
+    list_display = ('marca', 'model', 'matricula')  # Muestra estos campos en la lista
+    
+
+admin.site.register(Automobil, AutomobilAdmin)
--- lloguer/models.py
+++ lloguer/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.db import models
 
 class Automobil(models.Model):
     marca = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
-    matricula = models.CharField(max_length=10)
+    matricula = models.CharField(max_length=10, unique=True)  # Unique evita duplicados
+
+    def __str__(self):
+        return f"{self.marca} {self.model} ({self.matricula})"
 
