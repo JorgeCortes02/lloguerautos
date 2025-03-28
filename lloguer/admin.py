3331520453cee54007625efd9c4fe744f34fc7ed
Jorge Cortés <jorgecortes@MacBook-Pro-de-Jorge.local>
2025-03-28 16:16:12 +0100

    fita4

--- lloguer/admin.py
+++ lloguer/admin.py
@@ -3,16 +3,13 @@ from django.contrib import admin
 from .models import *
 
 class AutomobilAdmin(admin.ModelAdmin):
-    list_display = ('marca', 'model', 'matricula')  # Muestra estos campos en la lista
+    list_display = ('marca', 'model', 'matricula')  
     
 
 admin.site.register(Automobil, AutomobilAdmin)
 
 
 class ReservaAdmin(admin.ModelAdmin):
-    list_display = ('automobil', 'usuario', 'data_inici', 'data_fi')  # Muestra estos campos
-    search_fields = ('automobil__marca', 'usuario__username')  # Permite búsqueda
-    list_filter = ('data_inici', 'automobil')  # Filtros útiles
-    ordering = ('-data_inici',)  # Orden por fecha descendente
-
+    list_display = ('automobil', 'usuario', 'data_inici', 'data_fi') 
+    
 admin.site.register(Reserva, ReservaAdmin)
